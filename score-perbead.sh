a85274deadedd53c8b51cdf3aa52b31279faae97
Arha <kravchenko.anna.d@gmail.com>
2022-05-23 17:04:33 +0200

    Update score-perbead.sh
    enabled use in parallel; do not print if energy ==  0; fixed awk typo

--- score-perbead.sh
+++ score-perbead.sh
@@ -1,20 +1,24 @@
 rec=$1
 lig=$2
 
+tdir="$(mktemp -d /tmp/XXXXXXXXX)" || exit 1 # make a random dir in /tmp/ and save path to it 
+
 nlig=`cat $lig|wc -l`
 nrec=`cat $rec|wc -l`
 
 for i in `seq $nlig`; do
-	awk -v i=$i 'NR==i' $lig > /tmp/ligbead$i.pdb
+	awk -v i=$i 'NR==i' $lig > $tdir/ligbead$i.pdb
 done
 
 for i in `seq $nrec`; do
-	awk -v i=$i 'NR==i' $rec > /tmp/recbead$i.pdb
+	awk -v i=$i 'NR==i' $rec > $tdir/recbead$i.pdb
 done
 
 	
 for i in `seq $nrec`; do
 	for j in `seq $nlig`; do
-		$ATTRACTDIR/attract $ATTRACTDIR/../structure-single.dat $ATTRACTDIR/../attract.par /tmp/recbead$i.pdb /tmp/ligbead$j.pdb |awk -v i=$i j=$j '$2=="Energy:"{print i, j, $3}'
+		$ATTRACTDIR/attract $ATTRACTDIR/../structure-single.dat $ATTRACTDIR/../attract.par $tdir/recbead$i.pdb $tdir/ligbead$j.pdb |awk -v i=$i -v j=$j '$2=="Energy:"{ if($3!=0.000) print i, j, $3}'
 	done
 done
+
+rm -r $tdir
